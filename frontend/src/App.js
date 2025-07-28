51b388cc9dcd8790ee0d815c74c60fa33e556f9f
Rithvik <ritkot4@gmail.com>
2025-07-28 16:08:57 -0700

    integral

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FractureRiskForm from './FractureRiskForm';
+
+function App() {
+  return (
+    <div className="App">
+      <FractureRiskForm />
+    </div>
+  );
+}
+
+export default App;
